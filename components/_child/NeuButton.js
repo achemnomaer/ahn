499e3b235df8900f0477cb4ec5fa43e3c7465fef
Achem Habib Nomaer <achemnomaer57@gmail.com>
2024-07-12 23:15:33 +0600

    added my resume

--- components/_child/NeuButton.js
+++ components/_child/NeuButton.js
@@ -1,4 +1,5 @@
 import cn from "classnames";
+import Link from "next/link";
 
 export default function NeuButton({ children, light, dark, both, ...rest }) {
   const className = cn(
@@ -10,8 +11,12 @@ export default function NeuButton({ children, light, dark, both, ...rest }) {
     }
   );
   return (
-    <button {...rest} className={className}>
-      {children}
-    </button>
+    <Link href="/resume_achem.pdf" passHref legacyBehavior>
+      <a target="_blank">
+        <button {...rest} className={className}>
+          {children}
+        </button>
+      </a>
+    </Link>
   );
 }
